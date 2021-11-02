30aa560588f5b430faf1888b2663ff6c95bac115
Thomas Waaler <superxgenesis@gmail.com>
2021-11-02 12:16:09 +0100

    Something

--- Oblig5/main.py
+++ Oblig5/main.py
@@ -1,11 +1,29 @@
 import pygame
+from pygame.draw import rect
 from pygame.locals import *
+
 import json
+from dataclasses import dataclass
+
+# http://borgar.net/programs/sokoban/#Sokoban
 
 # Colours
+# -------
 TEAL = (0, 121, 107)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+# -------
+
+SPRITE_SIZE = 32
+
+# Dictionary of sprite positions in the spritesheet
+sprite_keys = {
+    " ": (0, 0),    # Floor
+    "#": (0, 13),   # Solid Wall
+    "@": (24, 0),   # Player
+    "B": (15, 14),  # Box
+    "G": (35, 12),  # Goal
+}
 
 def get_level_map(index: int) -> list:
     '''Returns the map data of given index.
@@ -18,11 +36,31 @@ def get_level_map(index: int) -> list:
     with open("level_data.json") as file:
         json_obj = json.load(file)
         
-        # Check if level index exist
         if index > len(json_obj):
             return None
-        
-        print(len(json_obj))
+
+        return json_obj[index - 1]['level_data']
+
+def get_sprite_map(level_data: list) -> list:
+    sprite_list = []
+    sprite_sheet = pygame.image.load("resources/colored_transparent_packed.png").convert_alpha()
+
+    for y in range( len(level_data) ):
+        for x in range( len(level_data[y]) ):
+            sprite_pos = sprite_keys[ level_data[y][x] ]
+
+            sprite = pygame.sprite.Sprite()
+            sprite.rect = pygame.Rect((sprite_pos[0] * SPRITE_SIZE, sprite_pos[1] * SPRITE_SIZE), (32, 32))   # Where in the spritesheet to get the image
+            sprite.image = pygame.Surface(sprite.rect.size, pygame.SRCALPHA)
+            sprite.image.blit(sprite_sheet, (0, 0), sprite.rect)
+
+            sprite.rect.topleft = (x * 32, y * 32)    # Position for screen
+            sprite_list.append(sprite)
+    
+    return sprite_list
+
+class Level():
+    pass
 
 
 class Player(pygame.sprite.Sprite):
@@ -51,10 +89,9 @@ class Game():
         self.display_surf = pygame.display.set_mode(self.size)
         pygame.display.set_caption(self.TITLE)
 
-        some_sprite = pygame.sprite.Sprite()
-        some_sprite.image = pygame.image.load("resources/player.png")
-        some_sprite.rect = some_sprite.image.get_rect()
-        some_sprite.rect.topleft = (200, 200)
+        
+
+        #my_level = get_sprite_map( get_level_map(2) )
 
         while self.is_running:
             # Go through all events that has arisen 
@@ -62,7 +99,8 @@ class Game():
                 self.on_event(event)
             
             self.display_surf.fill(TEAL)
-            self.display_surf.blit(some_sprite.image, some_sprite.rect)
+            #for sprt in my_level:
+            #    self.display_surf.blit(sprt.image, sprt.rect)
             
             # Render everything
             pygame.display.update()
@@ -75,6 +113,5 @@ class Game():
 
 # Main game entry
 if __name__ == "__main__":
-    get_level_map(0)
-    #sokoban = Game("Sokoban")
-    #sokoban.run()
+    sokoban = Game("Sokoban")
+    sokoban.run()
